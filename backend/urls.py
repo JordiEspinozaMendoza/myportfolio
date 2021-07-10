0766b87fc033fe299dfaf4a8634e04b320781256
Jordi <jordi8101@gmail.com>
2021-07-10 13:04:20 -0700

    pre prod

--- backend/urls.py
+++ backend/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html")),
     path("api/users/", include("base.urls.user_urls")),
     path("api/proyects/", include("base.urls.proyect_urls")),
     path("api/categories/", include("base.urls.categorie_urls")),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
