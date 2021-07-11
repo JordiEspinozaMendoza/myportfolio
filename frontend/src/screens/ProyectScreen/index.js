3fd82df8922d6157b0dc42558afab26afe827763
Jordi <jordi8101@gmail.com>
2021-07-10 18:40:40 -0700

    Description added

--- frontend/src/screens/ProyectScreen/index.js
+++ frontend/src/screens/ProyectScreen/index.js
@@ -49,10 +49,7 @@ export default function ProyectScreen({ match, history, location }) {
       <div className="proyect-container">
         <h2>{proyect?.name}</h2>
         <span>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
+          {proyect?.description}
         </span>
 
         <Grid container xs={5} className="buttons-container">
