3bf23bbd0e94ee3e2ff229ecadaed99f082528db
JordiEspinoza <71615543+JordiEspinozaMendoza@users.noreply.github.com>
2021-10-26 00:34:31 -0700

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -157,6 +157,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 cloudinary.config(
     cloud_name="jordiespinoza",
